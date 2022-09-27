7a4658d218399ca1465084bfe07133633e18db5c
Chris <chrisphilholt@gmail.com>
2022-09-27 16:13:05 +0100

    added lottery contract

---
+++ src/contracts/lotteryContract5.js
@@ -0,0 +1,191 @@
+const abi = [
+    {
+        "inputs": [],
+        "name": "enter",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "pickWinner",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "inputs": [],
+        "name": "getBalance",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getOwnersAddress",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getPlayers",
+        "outputs": [
+            {
+                "internalType": "address payable[]",
+                "name": "",
+                "type": "address[]"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getPlayersLength",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getRandomNumber",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "lottery",
+                "type": "uint256"
+            }
+        ],
+        "name": "getWinnerByLottery",
+        "outputs": [
+            {
+                "internalType": "address payable",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "isPoolFull",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "lotteryHistory",
+        "outputs": [
+            {
+                "internalType": "address payable",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "lotteryId",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "players",
+        "outputs": [
+            {
+                "internalType": "address payable",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
+
+const address = "0x28947dBbECeB0971B961dD30c34d56d3A49F6007";
+
+const createContract5 = web3 => {
+    return new web3.eth.Contract(abi, address);
+}
+
+
+export default createContract5;
--- src/pages/Pools.js
+++ src/pages/Pools.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import LotteryCard from '../components/LotteryCard';
 
-const Pools = () => {
+const Pools = ({ lotteryContract5, userObj, web3 }) => {
+
+    const [lotteryContract5Details, setLotteryContract5Players] = useState({
+        players: 0,
+        balance: 0,
+        poolFull: false,
+        address: "0xa43485521AbC7BCFA48556332A8cb4aaB000b4Bd"
+    });
+
+    const getContract5Info = async () => {
+        const players5 = await lotteryContract5.methods.getPlayersLength().call();
+        const balance5 = await lotteryContract5.methods.getBalance().call();
+        const poolFull = await lotteryContract5.methods.isPoolFull().call();
+        setLotteryContract5Players((prev) => {
+            return { ...prev, players: players5 }
+        })
+        setLotteryContract5Players((prev) => {
+            return { ...prev, balance: web3.utils.fromWei(balance5, 'ether') }
+        })
+        setLotteryContract5Players((prev) => {
+            return { ...prev, poolFull: poolFull }
+        })
+    }
+
+
+    useEffect(() => {
+        getContract5Info()
+    }, [])
+
+
     return (
-        <div>Pools</div>
+        <div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <LotteryCard contract={lotteryContract5} cardDetails={lotteryContract5Details} maxPlayers={5} userObj={userObj} priceInEther={0.011} web3={web3} />
+        </div>
     )
 }
 
