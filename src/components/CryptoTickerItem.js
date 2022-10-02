c49de9e7002e04a669ca73056b682700a2d9eb05
Lewis <lewisdsykes@gmail.com>
2022-10-02 20:03:29 +0100

    added styling on home page

--- src/components/CryptoTickerItem.js
+++ src/components/CryptoTickerItem.js
@@ -6,12 +6,11 @@ const CryptoTickerItem = ({name, price, change}) => {
 
 
 
-
     return(
 
         <>  
         <div className="item">
-            <h3>{name}</h3>
+            <h3>{name.toUpperCase()}</h3>
             <h3 className={change > 0 ? "green" : "red" }>{change}%</h3>
         </div>
 
