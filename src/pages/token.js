bb550683ee588f98379904af55a0fd13c7bc0b16
Chris <chrisphilholt@gmail.com>
2022-10-03 17:09:18 +0100

    added pic of logo

--- src/pages/token.js
+++ src/pages/token.js
@@ -3,18 +3,20 @@ import React from 'react'
 const Token = () => {
     return (
         <>
-        <img className='Token-Logo' src="/static/buddie-logo.svg"/>
-        <div className='total-supply'>
-            <h1>Total Supply:
-                1,000,000,000,000
-            </h1>
-        </div>
-        <div>
-            <h2>Welcome to the Royal Jelly Token</h2>
-            <p className='token-info'>
-                This is our coin, like Dogecoin, Royal Jelly is a fun token to see how a community can come together and GROW.
-            </p>
-        </div>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <img className='Token-Logo' src="/static/RJellyCoin.png" />
+                <div className='total-supply'>
+                    <h1>Total Supply:
+                        1,000,000,000,000
+                    </h1>
+                </div>
+                <div>
+                    <h2>Welcome to the Royal Jelly Token</h2>
+                    <p className='token-info'>
+                        This is our coin, like Dogecoin, Royal Jelly is a fun token to see how a community can come together and GROW.
+                    </p>
+                </div>
+            </div>
         </>
     )
 }
