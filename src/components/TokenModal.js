90f3e700251418012ace3c012fddef1e3e6cc76a
Lewis <lewisdsykes@gmail.com>
2022-10-03 12:42:09 +0100

    remove second search input

--- src/components/TokenModal.js
+++ src/components/TokenModal.js
@@ -88,9 +88,6 @@ function TokenModal({ tokenOpen, handleTokenModal, tokens, handleTokenFromChange
                     <button className="modal-exit" onClick={handleClose}><FontAwesomeIcon icon={faCircleXmark}/></button>
                     <input className="modal-search" autoFocus="true" type="text" placeholder='Search Library' value={tokenSearch} onChange={handleTokenSearch} />
 
-                    <button className='modal-exit' onClick={handleClose}><FontAwesomeIcon icon={faCircleXmark}/></button>
-                    <input autoFocus="true" type="text" placeholder='Search Library' value={tokenSearch} onChange={handleTokenSearch} />
-
                     <Typography id="modal-modal-title" variant="h6" component="h2">
                         Select a token
                     </Typography>
