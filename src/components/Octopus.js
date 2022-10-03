81f488a8ad16acf99146494c00a14e8b2686e97f
Lewis <lewisdsykes@gmail.com>
2022-10-03 10:19:46 +0100

    added gif for jellyfish

--- src/components/Octopus.js
+++ src/components/Octopus.js
@@ -7,7 +7,7 @@ export default function Octopus() {
   return (
     <>
     <div className='octopus-div'>
-      <iframe src='https://my.spline.design/untitled-af7e234bef0a2a24d362d1313dbc1b51/' frameborder='0' width='100%' height='100%' className='octopus'></iframe>
+      <img src="/static/jelly-fish.gif" style={{pointerEvents: 'none'}} className='octopus'></img>
     </div>
     </>  
 
