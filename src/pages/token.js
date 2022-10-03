db9e3dd72f7d3910e28a60898e6f87985ca3f17a
Naefunn <nathan.d.foster1@gmail.com>
2022-10-03 12:56:57 +0100

    added logo

--- src/pages/token.js
+++ src/pages/token.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const token = () => {
     return (
+        <>
+        <img src="/static/buddie-logo.svg"/>
         <div>Welcome to Chlorine Token</div>
+        </>
     )
 }
 
